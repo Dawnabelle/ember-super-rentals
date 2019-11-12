9750d23ef9fb060a006d89bbe79fc8dc789fd742
Dawn <dawn.mott@oregonmetro.gov>
2019-11-12 09:12:33 -0800

    add shopping-cart service

---
+++ app/components/cart-contents.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+
+  cart: service('shopping-cart'),
+
+  actions: {
+    remove(item) {
+      this.cart.remove(item);
+    }
+  }
+
+});
